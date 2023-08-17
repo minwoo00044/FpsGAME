12f174ce7912235ffc52272bd1adeae0f4b4a5b7
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-08-17 16:21:27 +0900

    feat 카메라,&플레이어의 회전, 플레이어 움직임&이동
    카메라 회전
    - CamRotation.cs 기능 추가
    플레이어 회전
    - PlayerRotation.cs 기능 추가
    플레이어 이동, 점프
    - PlayerMove.cs 기능 추가

---
+++ Assets/Scripts/CamRotate.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//마우스 입력 받아서 카메라를 회전
+//마우스입력 x,y, 속도
+//순서 1 : 사용자의 마우스 입력을 받는다.
+//순서 2 : 마우스 입력에 따라 회전 방향을 설정한다.
+//순서 3 : 물체를 회전시킨다.
+public class CamRotate : MonoBehaviour
+{
+    //마우스입력 x,y, 속도
+    public float speed = 200f;
+    float mx = 0;
+    float my = 0;
+    void Update()
+    {
+        //순서 1 : 사용자의 마우스 입력(x,y)을 받는다.
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        mx += mouseX * speed * Time.deltaTime;
+        my += mouseY * speed * Time.deltaTime;
+
+        my = Mathf.Clamp(my, -90, 90);
+        //순서 2 : 마우스 입력에 따라 회전 방향을 설정한다.
+        Vector3 dir = new Vector3(-my, mx, 0);
+        //순서 3 : 물체를 회전시킨다.
+        // r = r0 + vt
+
+        transform.eulerAngles = dir;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//목적 : wasd를 누르면 캐릭터를 그 방향으로 이동시키고 싶다.
+//필요속성 : 이동속도
+
+//목적2 : 스페이스 누르면 수직으로 점프하고 싶다.
+//필요속성 : 캐릭터 컨트롤러, 중력 변수, 수직 속력 변수. , 점프파워, 점프 상태 변수
+//2-1. 캐릭터 수직 속도에 중력을 적용하고 싶다.
+//2-2. 캐릭터 컨트롤러로 나를 이동시키고 싶다.
+//2-3 스페이스를 누르면 수직속도에 점프파워 적용하고 싶다.
+// 2-4 점프 중인지 확인하고 싶다.
+
+//목적3. 점프 중인지 확인하고 점프 중이면 점프전 상태로 초기화 하고 싶다.
+//그리고
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpPower = 10;
+    CharacterController characterController;
+    float gravity = -20f;
+    float yVelocity = 0;
+    public bool isJumping = false;
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //입력
+
+        if (isJumping && characterController.collisionFlags == CollisionFlags.Below)
+        {
+            isJumping = false;
+            
+        }
+        //바닥에 닿아있을때 수직 속도 초기화
+        else if (characterController.collisionFlags == CollisionFlags.Below)
+        {
+            yVelocity = 0;
+        }
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        //이동방향 설정
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir = Camera.main.transform.TransformDirection(dir);
+
+        //중력500배
+        yVelocity = yVelocity + gravity * Time.deltaTime;
+        dir.y = yVelocity;
+        print(yVelocity);
+        //만약 점프 중이었다면 점프 전 상태로 초기화 하고 싶다.
+        //이동
+        //transform.position += dir * speed * Time.deltaTime;
+        //2-2. 캐릭터 컨트롤러로 나를 이동시키고 싶다.
+        characterController.Move(dir * speed * Time.deltaTime);
+    }
+}
