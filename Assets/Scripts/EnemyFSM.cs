92cda06d3699c95f5406149985b66d07261a4e11
minwoo00044 <70777372+minwoo00044@users.noreply.github.com>
2023-08-21 13:35:54 +0900

    feat FSM
    enemFSM.cs
    idle, move 까지 구현

---
+++ Assets/Scripts/EnemyFSM.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//목표 : 적을 FSM 다이어그램에 따라 동작시키고 싶다.
+//필요속성 : 적 상태
+
+//목표2 : 플레이어와의 거리를 측정해서 특정 상태로 만들어준다.
+//필요 속성 : 플레이어와의 거리, 플레이어 트랜스폼
+
+//목표3 : 적의 상태가 Move일 때, 플레이어와의 거리가 공격범위 밖이면 적이 플레이어를 따라간다.
+//필요속성3: 이동 속도, 적의 이동을 위한 캐릭터 컨트롤러
+public class EnemyFSM : MonoBehaviour
+{
+    enum EnemyState
+    {
+        Idle,
+        Move,
+        Attack,
+        Return,
+        Damaged,
+        Die
+    }
+    EnemyState enemyState;
+    //필요 속성 : 플레이어와의 거리, 플레이어 트랜스폼
+    public float findDistance = 5f;
+    Transform player;
+    //필요속성3: 이동 속도, 적의 이동을 위한 캐릭터 컨트롤러, 공격 범위
+    public float moveSpeed;
+    CharacterController characterController;
+    public float attackDist = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyState = EnemyState.Idle;
+        player = GameObject.Find("Player").transform;
+        characterController = gameObject.GetComponent<CharacterController>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (enemyState)
+        {
+            case EnemyState.Idle:
+                Idle();
+                break;
+            case EnemyState.Move:
+                Move();
+                break;
+            case EnemyState.Attack:
+                Attack();
+                break;
+            case EnemyState.Return:
+                Return();
+                break;
+            case EnemyState.Damaged:
+                Damaged();
+                break;
+            case EnemyState.Die:
+                Die();
+                break;
+        }
+    }
+
+    private void Die()
+    {
+    }
+
+    private void Damaged()
+    {
+    }
+
+    private void Return()
+    {
+    }
+
+    private void Attack()
+    {
+
+    }
+    //목표3 : 적의 상태가 Move일 때, 플레이어와의 거리가 공격범위 밖이면 적이 플레이어를 따라간다.
+    private void Move()
+    {
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+        if (distanceToPlayer > attackDist)
+        {
+            Vector3 dir = (player.position - transform.position).normalized;
+            //플레이어를 따라간다.
+            characterController.Move(dir * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            //공격 범위로 들어오면 공격상태로 전환
+            enemyState = EnemyState.Attack;
+            print("상태전환 : Move to Attack");
+        }
+    }
+
+    private void Idle()
+    {
+        //목표2 : 플레이어와의 거리를 측정해서 특정 상태로 만들어준다.
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        //현재 플레이어와의 거리가 특정 범위 내면 상태를 Move로 바꾼다.
+        if (distanceToPlayer < findDistance) 
+        {
+            enemyState = EnemyState.Move;
+            print("상태전환 : Idle to Move");
+        }
+    }
+}
--- Assets/Scripts/PlayerFire.cs
+++ Assets/Scripts/PlayerFire.cs
@@ -6,21 +6,73 @@ using UnityEngine;
 //순서 1 마우스 우클릭
 //폭탄 게임오브젝트를 생성하고 firePosition에 위치시킨다.
 //폭탄의 리지드바디에 카메라 정면방향으로 힘을 가한다.
+//목적2: 마우스 왼쪽 버튼 누르면 시선 방향으로 총쏘기
+//2-2 레이를 생성하고 발사 위치와 발사 방향 설정
+//2-3 레이가 부딪힌 대상의 정보를 저장할 수 있는 변수 만든다.
+//2-4 레이를 발사하고, 부딪힌 물체가 있으면 그 위치에 피격 효과를 만든다.
+//필요속성 : 피격효과 게임오브젝트, 이펙트의 파티클 시스템.
 public class PlayerFire : MonoBehaviour
 {
     public GameObject bomb;
     public GameObject firePosition;
     public float power;
 
+    //필요속성 : 피격효과 게임오브젝트, 이펙트의 파티클 시스템.
+    public GameObject hitEffect;
+    ParticleSystem particleSystem0;
+    private void Start()
+    {
+        particleSystem0 = hitEffect.GetComponent<ParticleSystem>();
+        //int x = 3;
+        //int y = 4;
+        //Swap(ref x, ref y);
+        //print(string.Format("x : {0}, y:{1}", x, y));
+
+        //int a = 3;
+        //int b = 4;
+        //int quotint;
+        //int remainder;
+
+        //quotint = Divide(a, b, out remainder);
+        //print(string.Format("몫: {0}, 나머지: {1}", quotint, remainder));
+    }
+    
     void Update()
     {
         if(Input.GetMouseButtonDown(1))
         {
             GameObject instance = Instantiate(bomb);
             instance.transform.position = firePosition.transform.position;
-
             Rigidbody rb = instance.GetComponent<Rigidbody>();
             rb.AddForce(Camera.main.transform.forward * power, ForceMode.Impulse);
         }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hitInfo = new RaycastHit();
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                print("거리" + hitInfo.distance);
+                //부딪힌 물체 있으면 피격효과(법선 벡터 방향으로) 만들긔
+                hitEffect.transform.position = hitInfo.point;
+                hitEffect.transform.forward = hitInfo.normal;
+                //피격 이펙트 재생
+                particleSystem0.Play();
+            }
+        }
     }
+
+    //public void Swap(ref int a, ref int b)
+    //{
+    //    int temp = a;
+    //    a = b;
+    //    b = temp;
+    //}
+
+    //public int Divide(int a, int b, out int remainder)
+    //{
+    //    remainder = a % b;
+    //    return a / b;
+    //}
+
 }
